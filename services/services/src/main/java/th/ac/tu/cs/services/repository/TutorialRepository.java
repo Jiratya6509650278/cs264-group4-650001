89aeab254ddff49d4381c171b23f7bcfb575791e
Panapol6509650146 <panapol.jun@dome.tu.ac.th>
2023-11-09 21:10:55 +0700

    new file:   services/.idea/.gitignore 	new file:   services/.idea/compiler.xml 	new file:   services/.idea/encodings.xml 	new file:   services/.idea/jarRepositories.xml 	new file:   services/.idea/misc.xml 	new file:   services/.idea/modules.xml 	new file:   services/.idea/services.iml 	new file:   services/.idea/uiDesigner.xml 	new file:   services/services/.gitignore 	new file:   services/services/.mvn/wrapper/maven-wrapper.jar 	new file:   services/services/.mvn/wrapper/maven-wrapper.properties 	new file:   services/services/mvnw 	new file:   services/services/mvnw.cmd 	new file:   services/services/out/artifacts/services_jar/services.jar 	new file:   services/services/pom.xml 	new file:   services/services/src/main/java/th/ac/tu/cs/services/ServicesApplication.java 	new file:   services/services/src/main/java/th/ac/tu/cs/services/controller/TutorialController.java 	new file:   services/services/src/main/java/th/ac/tu/cs/services/model/Tutorial.java 	new file:   services/services/src/main/java/th/ac/tu/cs/services/repository/JdbcTutorialRepository.java 	new file:   services/services/src/main/java/th/ac/tu/cs/services/repository/TutorialRepository.java 	new file:   services/services/src/main/resources/META-INF/MANIFEST.MF 	new file:   services/services/src/main/resources/application.properties 	new file:   services/services/src/main/resources/static/Thammasat_Horz_logo_Red.png 	new file:   services/services/src/main/resources/static/Thammasat_Horz_logo_black.png 	new file:   services/services/src/main/resources/static/Thammasat_University.svg.png 	new file:   services/services/src/main/resources/static/index.html 	new file:   services/services/src/main/resources/static/index1.html 	new file:   services/services/src/main/resources/static/index2.html 	new file:   services/services/src/main/resources/static/script.js 	new file:   services/services/src/main/resources/static/scriptte.js 	new file:   services/services/src/main/resources/static/styles.css 	new file:   services/services/src/main/resources/static/styleste.css 	new file:   services/services/src/main/resources/static/user (1).png 	new file:   services/services/src/test/java/th/ac/tu/cs/services/ServicesApplicationTests.java

---
+++ services/services/src/main/java/th/ac/tu/cs/services/repository/TutorialRepository.java
@@ -0,0 +1,8 @@
+package th.ac.tu.cs.services.repository;
+import java.util.List;
+import th.ac.tu.cs.services.model.Tutorial;
+
+public interface TutorialRepository {
+    int save(Tutorial book);
+
+}
